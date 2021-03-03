a521afd6435b1aa8fd1b30b20be78de4d76a2d94
bryaniiiii <51448923+bryaniiiii@users.noreply.github.com>
2021-03-03 16:36:21 +0800

    Initial commit

---
+++ app/src/main/java/com/example/mywifiapp2/MainActivity.java
@@ -0,0 +1,108 @@
+package com.example.mywifiapp2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WifiManager wifiManager ;
+    private ListView listView;
+    private Button buttonScan;
+    private int size =0;
+    private List<ScanResult> results;
+    private ArrayList<String> arrayList = new ArrayList<>();
+    private ArrayAdapter adapter;
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        buttonScan = findViewById(R.id.scan);
+        buttonScan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                scanWifi();
+            }
+        });
+        listView = findViewById(R.id.wifiList);
+        wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+
+        if (!wifiManager.isWifiEnabled()) {
+            Toast.makeText(this, "Wifi is disabled", Toast.LENGTH_LONG).show();
+            wifiManager.setWifiEnabled(true);
+        }
+
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, arrayList);
+        listView.setAdapter(adapter);
+
+
+        scanWifi();
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                myRef.child("SSID").child(results.get(position).SSID).setValue(results.get(position).SSID);
+                myRef.child("SSID").child(results.get(position).SSID).child("BSSID").setValue(results.get(position).BSSID);
+                myRef.child("SSID").child(results.get(position).SSID).child("Description").setValue(results.get(position).capabilities);
+                myRef.child("SSID").child(results.get(position).SSID).child("RSSI").setValue(results.get(position).level);
+
+            }
+        });
+
+
+
+    }
+
+    private void scanWifi() {
+        if (!wifiManager.isScanThrottleEnabled()) {
+            // scan without any restrictions
+
+            arrayList.clear();
+            registerReceiver(wifiReceiver, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
+
+            wifiManager.startScan();
+            Toast.makeText(this, "Scanning Wifi...", Toast.LENGTH_SHORT).show();
+
+        }
+
+    }
+
+    BroadcastReceiver wifiReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            results = wifiManager.getScanResults();
+            unregisterReceiver(this);
+
+            for (ScanResult scanResult : results) {
+                arrayList.add(" BSSID: \n" + scanResult.BSSID + "\n" + " SSID: \n" + scanResult.SSID+ "\n" + " Description: \n" + scanResult.capabilities +"\n" + " RSSI: \n" + scanResult.level);
+                adapter.notifyDataSetChanged();
+            }
+        }
+    };
+
+
+
+
+}
